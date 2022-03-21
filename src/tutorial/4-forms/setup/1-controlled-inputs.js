7ebc64d68439c82a7011d206279f8a3f6bee8bbe
Luiz Fernando <luizfernandorg@gmail.com>
2022-03-21 17:12:15 -0300

    Edit the controlled-inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,20 +8,28 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
+  const [people, setPeople] = useState([])
   
-  const checkValue = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    const input = document.getElementById('firstName')
-    const email = document.getElementById("email")
-    console.log(input.value)
-    console.log(email.value)
-    input.value = ""
-    email.value = ""
+    if(firstName && email){
+      let person = { id: new Date().getTime().toString(), firstName, email}
+      setPeople((people) => {
+        people.push(person)
+        return people
+      })
+      
+      console.log(people)
+    }else{
+      console.log("empty values")
+    }
+    setFirstName("")
+    setEmail("")
   }
-
+  
 return <>
     <article>
-      <form className='form'>
+      <form className='form' onSubmit={(e) => handleSubmit(e)}>
         <div className='form-control'>
           <label htmlFor='firstName'>Name : </label>
           <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
@@ -30,9 +38,20 @@ return <>
           <label htmlFor='email'>Email : </label>
           <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
         </div>
-        <button type='submit' onClick={checkValue}>Enviar</button>
+        <button type='submit'>Enviar</button>
       </form>
     </article>
+    {
+      people.map((person)=>{
+        const {id,firstName,email} = person
+        return (
+          <div key={id} className="item">
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+          </div>
+        )
+      })
+    }
   </>;
 };
 
